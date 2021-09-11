3825c12a83eee183b4fa447d525ce7b7f31ff75b
Hansani-999 <it19965000@my.sliit.lk>
2021-09-11 11:01:46 +0530

    Validate with yup in updateBoost view

--- src/views/examples/UpdateBoostEvent.js
+++ src/views/examples/UpdateBoostEvent.js
@@ -33,6 +33,7 @@ const UpdateBoostEvent = () => {
     advertisement_Pic: "",
   };
 
+  //Yup validations
   const validationSchema = Yup.object({
     service_Provider_Name: Yup.string().required("Required"),
     contact_Number_SP: Yup.string().required("Required"),
