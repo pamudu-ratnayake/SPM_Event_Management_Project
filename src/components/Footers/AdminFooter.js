54d91a05bfc58c3cf10eacb8f7f192c742db143c
Malith Madusankha <malithsony@gmail.com>
2021-09-30 02:09:01 +0530

    Created dashboard and add componet

--- src/components/Footers/AdminFooter.js
+++ src/components/Footers/AdminFooter.js
@@ -1,62 +1,33 @@
-
-
 // reactstrap components
 import { Container, Row, Col, Nav, NavItem, NavLink } from "reactstrap";
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <Row className="align-items-center justify-content-xl-between">
-        <Col xl="6">
-          <div className="copyright text-center text-xl-left text-muted">
-            © {new Date().getFullYear()}{" "}
-            <a
-              className="font-weight-bold ml-1"
-              target="_blank"
-            >
-              Hex Clan
-            </a>
-          </div>
-        </Col>
+	return (
+		<footer className="footer">
+			<Row className="align-items-center justify-content-xl-between">
+				<Col xl="6">
+					<div className="copyright text-center text-xl-left text-muted">
+						© {new Date().getFullYear()}{" "}
+						<a className="font-weight-bold ml-1" target="_blank">
+							THE BLISS
+						</a>
+					</div>
+				</Col>
 
-        <Col xl="6">
-          <Nav className="nav-footer justify-content-center justify-content-xl-end">
-            <NavItem>
-              <NavLink
-                target="_blank"
-              >
-                Hex Clan
-              </NavLink>
-            </NavItem>
+				<Col xl="6">
+					<Nav className="nav-footer justify-content-center justify-content-xl-end">
+						<NavItem>
+							<NavLink target="_blank">THE BLISS</NavLink>
+						</NavItem>
 
-            <NavItem>
-              <NavLink
-                target="_blank"
-              >
-                About Us
-              </NavLink>
-            </NavItem>
-
-            {/* <NavItem>
-              <NavLink
-                target="_blank"
-              >
-                Blog
-              </NavLink>
-            </NavItem> */}
-
-            {/* <NavItem>
-              <NavLink
-                target="_blank"
-              >
-                MIT License
-              </NavLink>
-            </NavItem> */}
-          </Nav>
-        </Col>
-      </Row>
-    </footer>
-  );
+						<NavItem>
+							<NavLink target="_blank">About Us</NavLink>
+						</NavItem>
+					</Nav>
+				</Col>
+			</Row>
+		</footer>
+	);
 };
 
 export default Footer;
